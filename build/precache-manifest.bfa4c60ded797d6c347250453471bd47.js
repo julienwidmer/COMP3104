65ffd75aaba603fb065e5a5eb9be149df8051e8a
Julien Widmer <ju.widmer@pm.me>
2022-10-03 20:30:36 -0400

    run npm build

---
+++ build/precache-manifest.bfa4c60ded797d6c347250453471bd47.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "762d171792fbb3929720",
+    "url": "/julienwidmer/COMP3104/static/css/main.d81b4f54.chunk.css"
+  },
+  {
+    "revision": "762d171792fbb3929720",
+    "url": "/julienwidmer/COMP3104/static/js/main.762d1717.chunk.js"
+  },
+  {
+    "revision": "835e0138cdf953673330",
+    "url": "/julienwidmer/COMP3104/static/js/runtime~main.835e0138.js"
+  },
+  {
+    "revision": "8ebec04f6c698faf83f8",
+    "url": "/julienwidmer/COMP3104/static/js/2.8ebec04f.chunk.js"
+  },
+  {
+    "revision": "5e6f21d418f0de864a1ecf542d55dbeb",
+    "url": "/julienwidmer/COMP3104/index.html"
+  }
+];
